2e387a7074444a873faaa8d6c1e3d5294ed55554
AkshayPatel-360 <axaymow@gmail.com>
2022-01-14 18:45:56 -0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,8 @@
 
-require('@nomiclabs/hardhat-waffle');
+// require('@nomiclabs/hardhat-waffle');
 
-module.export = {
-  solidity:'0.8.0',
-  networks: {
-    hardhat: {
-    },
-    ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/u430rbZG2sQueJu8EbzFHLq4rq00XWQp",
-      accounts: ['6b4094eab419c56b3f25a05fbae88b6b946f16030cf38dcc47a765eed1e49ddb']
-    }
-  }
-}
-
-
-// module.exports = {
-//   defaultNetwork: "ropsten",
+// module.export = {
+//   solidity:'0.8.0',
 //   networks: {
 //     hardhat: {
 //     },
@@ -23,23 +10,36 @@ module.export = {
 //       url: "https://eth-ropsten.alchemyapi.io/v2/u430rbZG2sQueJu8EbzFHLq4rq00XWQp",
 //       accounts: ['6b4094eab419c56b3f25a05fbae88b6b946f16030cf38dcc47a765eed1e49ddb']
 //     }
-//   },
-//   solidity: {
-//     version: "0.8.0",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200
-//       }
-//     }
-//   },
-//   paths: {
-//     sources: "./contracts",
-//     tests: "./test",
-//     cache: "./cache",
-//     artifacts: "./artifacts"
-//   },
-//   mocha: {
-//     timeout: 40000
 //   }
 // }
+
+
+ module.exports = {
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {
+    },
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/u430rbZG2sQueJu8EbzFHLq4rq00XWQp",
+      accounts: ['6b4094eab419c56b3f25a05fbae88b6b946f16030cf38dcc47a765eed1e49ddb']
+    }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
