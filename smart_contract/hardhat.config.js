e081d604496b82e6ddd7ae95ea97234f8e7f4f01
AkshayPatel-360 <axaymow@gmail.com>
2022-01-17 20:31:22 -0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,17 +1,5 @@
 
-// require('@nomiclabs/hardhat-waffle');
-
-// module.export = {
-//   solidity:'0.8.0',
-//   networks: {
-//     hardhat: {
-//     },
-//     ropsten: {
-//       url: "https://eth-ropsten.alchemyapi.io/v2/u430rbZG2sQueJu8EbzFHLq4rq00XWQp",
-//       accounts: ['6b4094eab419c56b3f25a05fbae88b6b946f16030cf38dcc47a765eed1e49ddb']
-//     }
-//   }
-// }
+KU
 
 
  module.exports = {
