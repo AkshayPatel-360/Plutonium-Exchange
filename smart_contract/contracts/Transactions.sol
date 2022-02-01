a0ea434945bad6e47130b456f4812e92ae4a9ab8
AkshayPatel-360 <axaymow@gmail.com>
2022-01-31 22:29:21 -0500

    Update Transactions.sol

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,34 +4,63 @@ pragma solidity ^0.8.0;
 contract Transactions {
     uint256 transactionCount;
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timeststamp, string keyword);
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timeststamp,
+        string keyword
+    );
 
-    struct TransferStruct{
+    struct TransferStruct {
         address sender;
         address receiver;
-        uint amount;
+        uint256 amount;
         string message;
-        uint timestamp;
+        uint256 timestamp;
         string keyword;
-
-    
     }
 
     TransferStruct[] transactions;
 
-    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public{
-        transactionCount += 1 ;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword); 
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
     }
-    
 
-    function getAllTransactions() public view returns (TransferStruct[] memory){
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
         return transactions;
     }
 
-    function getTransactionCount() public view returns (uint256){
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
 }
